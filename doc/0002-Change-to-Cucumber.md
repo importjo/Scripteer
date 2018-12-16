1e6e98e08a4adac187cbd9f150fe527f73cbd07e
Jonathan Battisti <jonathan.battisti.1@gmail.com>
2018-12-15 23:56:39 -0500

    Add ARD 0002-Change-to-Cucumber

---
+++ doc/0002-Change-to-Cucumber.md
@@ -0,0 +1,22 @@
+# 2. Change to Cucumber
+
+Date: 2018-12-15
+
+## Status
+
+Accepted
+
+## Context
+
+With the discovery of Cucumber for BDD, i wanted to add this feature in the project to test
+BDD programming. The original project gogo was already started in a blank Maven project with
+jdk 9 and junit 5 not fully compatible with Cucumber.
+
+## Decision
+
+I have created a new git repository name Scripteer and created a new Maven project from Cucumber archetype.
+Already created files will be copied into this project.
+
+## Consequences
+
+Further work in Scripteer are now done in BDD.
--- src/test/java/scripteer/Stepdefs.java
+++ src/test/java/scripteer/Stepdefs.java
@@ -1,34 +1,32 @@
 package scripteer;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 public class Stepdefs {
+	
+	private String[] params;
+	
 	@Given("^a sql query is set as a parameter$")
-	public void a_sql_query_is_set_as_a_parameter() throws Exception {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void a_sql_query_is_set_as_a_parameter() throws Exception { 
+		this.params[0] = "UPDATE testTable SET Nom = $A WHERE Id = $B";
 	}
 
 	@Given("^a source file is set as a parameter$")
 	public void a_source_file_is_set_as_a_parameter() throws Exception {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 
 	@When("^I run the program$")
 	public void i_run_the_program() throws Exception {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 
 	@Then("^a sql script should be created$")
 	public void a_sql_script_should_be_created() throws Exception {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    assertTrue(new File("test.sql").exists());
 	}
 
 }
